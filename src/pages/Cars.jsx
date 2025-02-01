739e8ea784342a23ea007f3a3dc8c3fa0aa501c9
Muhless <nuryadi.muhta@gmail.com>
2025-02-01 16:41:26 +0700

    update

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -5,7 +5,7 @@ import {
   Marker,
   Popup,
   Polyline,
-  useMap
+  useMap,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { Helmet } from "react-helmet-async";
@@ -34,13 +34,23 @@ const geocodeAddress = async (address) => {
 
 const fetchRoute = async (start, end, setRoute) => {
   try {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; // Proxy CORS
-    const targetUrl = `https://api.openrouteservice.org/v2/directions/driving-car?api_key=${ORS_API_KEY}&start=${start.lng},${start.lat}&end=${end.lng},${end.lat}`;
-
-    const response = await fetch(proxyUrl + targetUrl);
+    const response = await fetch("http://localhost:8080/getRoute", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        start_lat: start.lat,
+        start_lang: start.lang,
+        end_lat: end.lat,
+        end_lang: end.lang,
+      }),
+    });
     const data = await response.json();
     if (data.routes && data.routes.length > 0) {
-      const routeCoords = data.routes[0].geometry.coordinates.map(([lng, lat]) => ({ lat, lng }));
+      const routeCoords = data.routes[0].geometry.coordinates.map(
+        ([lng, lat]) => ({ lat, lng })
+      );
       setRoute(routeCoords);
     }
   } catch (error) {
@@ -125,7 +135,9 @@ const CarPages = () => {
       </Helmet>
       <div className="flex mt-6 container mx-auto items-center">
         <div className="w-1/3 px-5">
-          <h3 className="text-3xl font-semibold font-jakarta">Titik Awal dan Tujuan</h3>
+          <h3 className="text-3xl font-semibold font-jakarta">
+            Titik Awal dan Tujuan
+          </h3>
           <div className="mt-2">
             <h4 className="text-md font-semibold">Titik Awal</h4>
             <input
@@ -135,7 +147,10 @@ const CarPages = () => {
               onChange={(e) => setAddress(e.target.value)}
               className="p-2 border rounded mt-2 w-full"
             />
-            <button onClick={handleSetStartPoint} className="mt-2 w-full py-2 bg-blue-500 text-white rounded">
+            <button
+              onClick={handleSetStartPoint}
+              className="mt-2 w-full py-2 bg-blue-500 text-white rounded"
+            >
               Set Titik Awal
             </button>
           </div>
@@ -148,26 +163,47 @@ const CarPages = () => {
               onChange={(e) => setEndAddress(e.target.value)}
               className="p-2 border rounded mt-2 w-full"
             />
-            <button onClick={handleSetEndPoint} className="mt-2 w-full py-2 bg-blue-500 text-white rounded">
+            <button
+              onClick={handleSetEndPoint}
+              className="mt-2 w-full py-2 bg-blue-500 text-white rounded"
+            >
               Set Titik Tujuan
             </button>
           </div>
           {estimatedTime !== null && (
-            <p className="mt-4 text-md text-gray-500">Perkiraan Waktu Tempuh: {estimatedTime} menit</p>
+            <p className="mt-4 text-md text-gray-500">
+              Perkiraan Waktu Tempuh: {estimatedTime} menit
+            </p>
           )}
           <div className="mt-5">
-            <button onClick={handleResetPoints} className="p-2 w-full bg-red-500 text-white rounded">
+            <button
+              onClick={handleResetPoints}
+              className="p-2 w-full bg-red-500 text-white rounded"
+            >
               Reset
             </button>
           </div>
         </div>
         <div className="w-full h-full">
           {location && (
-            <MapContainer center={location} zoom={13} style={{ height: "600px", width: "100%" }} className="mt-10">
+            <MapContainer
+              center={location}
+              zoom={13}
+              style={{ height: "600px", width: "100%" }}
+              className="mt-10"
+            >
               <MapViewUpdater position={focusPoint || location} />
               <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-              {startPoint && <Marker position={startPoint}><Popup>Titik Awal</Popup></Marker>}
-              {endPoint && <Marker position={endPoint}><Popup>Titik Tujuan</Popup></Marker>}
+              {startPoint && (
+                <Marker position={startPoint}>
+                  <Popup>Titik Awal</Popup>
+                </Marker>
+              )}
+              {endPoint && (
+                <Marker position={endPoint}>
+                  <Popup>Titik Tujuan</Popup>
+                </Marker>
+              )}
               {route && <Polyline positions={route} color="blue" />}
             </MapContainer>
           )}
