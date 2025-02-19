f8ce23734db63f4f75205be5797314da0bf0614e
Muhless <nuryadi.muhta@gmail.com>
2025-02-19 10:26:54 +0700

    update car pages

--- src/pages/Admin/Car/Index.jsx
+++ src/pages/Admin/Car/Index.jsx
@@ -3,6 +3,14 @@ import { Helmet } from "react-helmet-async";
 import CarCard from "../../../components/Card/CarCard";
 import ListCarCard from "../../../components/Card/ListCarCard";
 
+const carTypes = [
+  "Pick Up",
+  "Colt Diesel Engkel",
+  "Colt Double Diesel",
+  "Fuso",
+  "Wingbox",
+];
+
 function CarPages() {
   return (
     <>
@@ -14,18 +22,22 @@ function CarPages() {
         />
       </Helmet>
       <div className="bg-card rounded-xl p-5">
-        <div className="grid grid-cols-3 gap-5">
-          <div className="col-span-1">
-            <CarCard />
-            <CarCard />
-            <CarCard />
-            <CarCard />
-          </div>
-          <div className="col-span-2">
-            <ListCarCard />
-            <ListCarCard />
-            <ListCarCard />
-          </div>
+        Daftar Mobil
+        <div className="flex justify-center gap-3">
+          {carTypes.map((type, index) => (
+            <div
+              key={index}
+              className="bg-card text-text h-8 flex items-center rounded-xl justify-center w-44 cursor-pointer border border-black hover:bg-text hover:text-card  hover:transition-all ease-in-out hover:duration-300"
+            >
+              <h1>{type}</h1>
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-between mt-10">
+          <CarCard />
+          <CarCard />
+          <CarCard />
+          <CarCard />
         </div>
       </div>
     </>
